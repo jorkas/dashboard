498793d0eb080695c26e06c234ff120c98662258
Jonas Forsberg <himynameisjonas@gmail.com>
2010-10-13 11:38:54 +0200

    added top countries

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,7 +6,7 @@ class DashboardController < ApplicationController
     @visits = get_visits
     @signup_journalists = get_signup_journalists
     @signup_follows = get_signup_follows
-    
+    @top_countries = get_top_countries
   end
   
   private
@@ -30,5 +30,9 @@ class DashboardController < ApplicationController
       last = Garb::Report.new(@profile, {:metrics => [:goal7Completions], :start_date => Time.now - 8.day, :end_date => Time.now - 8.day}).results.first.goal7_completions.to_i
       percent = (now.to_f/last.to_f)*100 - 100 
       {:now => now, :last => last, :percent => percent}
+    end
+    
+    def get_top_countries
+      Garb::Report.new(@profile, {:sort => :visits.desc, :limit => 10, :dimensions => [:country], :metrics => [:visits], :start_date => Time.now - 1.month - 1.day, :end_date => Time.now - 1.day }).results
     end
 end
