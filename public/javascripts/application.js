e24dc8e5bfd613a2e027a720bce17668ebf8e067
Jonas Forsberg <himynameisjonas@gmail.com>
2010-10-14 11:49:50 +0200

    time to live for widgets = reloading!

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,21 +3,29 @@
 
 
 $(document).ready(function(){
-  load_widget("total_visits")
-  load_widget("signup_journalists")
-  load_widget("signup_follows")
-  load_widget("signup_trails")
-  load_widget("top_searches")
-  load_widget("top_countries")
+  load_widget("total_visits", 1200000)
+  load_widget("signup_journalists", 1200000)
+  load_widget("signup_follows",1200000)
+  load_widget("signup_trails",1200000)
+  load_widget("top_searches",1200000)
+  load_widget("top_countries",1200000)
 })
 
 
-
-function load_widget (name) {
+function load_widget (name,ttl) {
+  console.log("loading_widget: "+name+" ttl: "+ttl)
   $.get("/widgets/"+name, function(data) {
     div = $("#"+name)
-    $(".spinner",div).fadeOut('slow', function() {
-        div.html(data);
-    });
+    if ( $(".spinner",div).size() > 0) {
+      $(".spinner",div).fadeOut('slow', function() {
+          div.html(data);
+      });
+    } else {
+      div.html(data);
+    }
+    setTimeout(function(){
+      console.log("reloading "+name)
+      load_widget(name,ttl)
+    },ttl);
   });
 }
