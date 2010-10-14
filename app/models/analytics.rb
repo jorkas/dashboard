912e89f567f2ffbbe4d5c390e8fb893b87e18552
Jonas Forsberg <himynameisjonas@gmail.com>
2010-10-14 12:10:21 +0200

    now using memcache for google analytics requests

--- app/models/analytics.rb
+++ app/models/analytics.rb
@@ -6,58 +6,81 @@ class Analytics
   end
   
   def total_visits
-    last = Garb::Report.new(@profile, {:metrics => [:visits], :start_date => Time.now - 2.month - 1.day, :end_date => Time.now - 1.month - 1.day}).results.first.visits.to_i
-    now = Garb::Report.new(@profile, {:metrics => [:visits], :start_date => Time.now - 1.month - 1.day, :end_date => Time.now - 1.day }).results.first.visits.to_i
+    last = Rails.cache.fetch("garb_total_visits_last", :expires_in => 1.hour) do
+      Garb::Report.new(@profile, {:metrics => [:visits], :start_date => Time.now - 2.month - 1.day, :end_date => Time.now - 1.month - 1.day}).results.first.visits.to_i
+    end
+    now = Rails.cache.fetch("garb_total_visits_now", :expires_in => 1.hour) do
+      Garb::Report.new(@profile, {:metrics => [:visits], :start_date => Time.now - 1.month - 1.day, :end_date => Time.now - 1.day }).results.first.visits.to_i
+    end
     percent = (now.to_f/last.to_f)*100 - 100 
     {:now => now, :last => last, :percent => percent}
   end
   
   def signup_journalists
-    now = Garb::Report.new(@profile, {:metrics => [:goal6Completions], :start_date => Time.now - 1.day, :end_date => Time.now - 1.day}).results.first.goal6_completions.to_i
-    last = Garb::Report.new(@profile, {:metrics => [:goal6Completions], :start_date => Time.now - 8.day, :end_date => Time.now - 8.day}).results.first.goal6_completions.to_i
-    percent = (now.to_f/last.to_f)*100 - 100 
-    {:now => now, :last => last, :percent => percent}
-  end
-  
-  def signup_follows  
-    now = Garb::Report.new(@profile, {:metrics => [:goal7Completions], :start_date => Time.now - 1.day, :end_date => Time.now - 1.day}).results.first.goal7_completions.to_i
-    last = Garb::Report.new(@profile, {:metrics => [:goal7Completions], :start_date => Time.now - 8.day, :end_date => Time.now - 8.day}).results.first.goal7_completions.to_i
-    percent = (now.to_f/last.to_f)*100 - 100 
-    {:now => now, :last => last, :percent => percent}
-  end
-  
-  def signup_trails  
-    now = Garb::Report.new(@profile, {:metrics => [:goal1Completions], :start_date => Time.now - 1.day, :end_date => Time.now - 1.day}).results.first.goal1_completions.to_i
-    last = Garb::Report.new(@profile, {:metrics => [:goal1Completions], :start_date => Time.now - 8.day, :end_date => Time.now - 8.day}).results.first.goal1_completions.to_i
-    percent = (now.to_f/last.to_f)*100 - 100 
-    {:now => now, :last => last, :percent => percent}
-  end
-  
-  def top_countries
-    Garb::Report.new(@profile, {:sort => :visits.desc, :limit => 10, :dimensions => [:country], :metrics => [:visits], :start_date => Time.now - 1.month - 1.day, :end_date => Time.now - 1.day }).results
-  end
-  
-  def top_searches
-    report = Garb::Report.new(@profile,
-                   :limit => 20,
-                   :start_date => (Date.today - 7.day),
-                   :end_date => (Date.today + 1.day))
-    report.metrics :visits
-    report.dimensions :pagePath
-    report.sort :visits.desc
-    report.filters do
-      contains(:page_path, 'query=')
-      contains(:page_path, '/search/')
-      does_not_contain(:page_path, 'query=&')
+      last = Rails.cache.fetch("garb_signup_jouenalists_last", :expires_in => 1.hour) do
+        Garb::Report.new(@profile, {:metrics => [:goal6Completions], :start_date => Time.now - 8.day, :end_date => Time.now - 8.day}).results.first.goal6_completions.to_i
+      end
+      now = Rails.cache.fetch("garb_signup_jouenalists_now", :expires_in => 1.hour) do
+        Garb::Report.new(@profile, {:metrics => [:goal6Completions], :start_date => Time.now - 1.day, :end_date => Time.now - 1.day}).results.first.goal6_completions.to_i
+      end
+      percent = (now.to_f/last.to_f)*100 - 100 
+      {:now => now, :last => last, :percent => percent}
     end
     
-    searchterms = Array.new
-    
-    report.results.each do |result|
-      search_term = CGI.parse(result.page_path)['query'].to_s
-      searchterms << {:search_term => search_term, :visits => result.visits.to_i } unless search_term.empty?
+    def signup_follows
+      last = Rails.cache.fetch("garb_signup_follows_last", :expires_in => 1.hour) do
+        Garb::Report.new(@profile, {:metrics => [:goal7Completions], :start_date => Time.now - 8.day, :end_date => Time.now - 8.day}).results.first.goal7_completions.to_i
+      end
+      now = Rails.cache.fetch("garb_signup_follows_now", :expires_in => 1.hour) do
+        Garb::Report.new(@profile, {:metrics => [:goal7Completions], :start_date => Time.now - 1.day, :end_date => Time.now - 1.day}).results.first.goal7_completions.to_i
+      end
+      percent = (now.to_f/last.to_f)*100 - 100 
+      {:now => now, :last => last, :percent => percent}
     end
-    searchterms[0...10]
-  end
+    
+    def signup_trails
+      last = Rails.cache.fetch("garb_signup_trails_last", :expires_in => 1.hour) do
+        Garb::Report.new(@profile, {:metrics => [:goal1Completions], :start_date => Time.now - 8.day, :end_date => Time.now - 8.day}).results.first.goal1_completions.to_i
+      end
+      now = Rails.cache.fetch("garb_signup_trails_now", :expires_in => 1.hour) do
+        Garb::Report.new(@profile, {:metrics => [:goal1Completions], :start_date => Time.now - 1.day, :end_date => Time.now - 1.day}).results.first.goal1_completions.to_i
+      end
+      percent = (now.to_f/last.to_f)*100 - 100 
+      {:now => now, :last => last, :percent => percent}
+    end
+    
+    def top_countries
+      results = Rails.cache.fetch("garb_top_countries", :expires_in => 1.hour) do
+        Garb::Report.new(@profile, {:sort => :visits.desc, :limit => 10, :dimensions => [:country], :metrics => [:visits], :start_date => Time.now - 1.month - 1.day, :end_date => Time.now - 1.day }).results
+      end
+      results
+    end
+    
+    def top_searches
+      results = Rails.cache.fetch("garb_top_searches", :expires_in => 1.hour) do
+        report = Garb::Report.new(@profile,
+                       :limit => 20,
+                       :start_date => (Date.today - 7.day),
+                       :end_date => (Date.today + 1.day))
+        report.metrics :visits
+        report.dimensions :pagePath
+        report.sort :visits.desc
+        report.filters do
+          contains(:page_path, 'query=')
+          contains(:page_path, '/search/')
+          does_not_contain(:page_path, 'query=&')
+        end
+        report.results
+      end
+      
+      
+      searchterms = Array.new
+      
+      results.each do |result|
+        search_term = CGI.parse(result.page_path)['query'].to_s
+        searchterms << {:search_term => search_term, :visits => result.visits.to_i } unless search_term.empty?
+      end
+      searchterms[0...10]
+    end
   
 end
