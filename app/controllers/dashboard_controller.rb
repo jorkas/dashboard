5142f65219fa4e547f7308e1a464dea93835e8b9
Jonas Forsberg <himynameisjonas@gmail.com>
2010-10-15 13:29:27 +0200

    removed debug-loggin

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,15 +5,9 @@ class DashboardController < ApplicationController
   
   def pshb
     if !params["hub.challenge"].nil?
-      logger.info "%%%%%%%%%%%%%%%"
-      logger.info request.inspect
-      logger.info "%%%%%%%%%%%%%%%"
       @challenge = params["hub.challenge"]
       render :action => "pshb", :status => 200, :layout => false
     else
-      logger.info "%%%%%%%%%%%%%%%"
-      logger.info request.body.read
-      logger.info "%%%%%%%%%%%%%%%"
       @challenge = "uhoh!"
       render :action => "pshb", :status => 404, :layout => false
     end
