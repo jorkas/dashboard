525eb2aa03105df9a1ed21a0009fdb35f39c6d70
Jonas Forsberg <himynameisjonas@gmail.com>
2010-10-15 11:42:19 +0200

    log psub-info

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,7 @@ class DashboardController < ApplicationController
   
   def pshb
     if !params["hub.challenge"].nil?
+      logger.info params["hub.challenge"]
       @challenge = params["hub.challenge"]
       render :action => "pshb", :status => 200, :layout => false
     else
