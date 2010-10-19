087233855e79cbe1a38439f83bca1da2911c778c
Jonas Forsberg <himynameisjonas@gmail.com>
2010-10-19 09:40:10 +0200

    added date_in_timezone method for correct time zone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :date_in_timezone
+  
+  def date_in_timezone
+    DateTime.now.in_time_zone.to_date
+  end
 end
