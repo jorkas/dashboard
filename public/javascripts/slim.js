d2d29655f0b8bfe170acd1921df2991be15a958b
Joakim Westerlund <joakim@newsdesk.se>
2010-12-09 12:03:56 +0100

    implemented rendering of bars from values...

--- public/javascripts/slim.js
+++ public/javascripts/slim.js
@@ -24,7 +24,10 @@ Dashboard.app = (function(){
             barBg.css("margin-top",320 - val);
         });
     };
-    var getActiveGraphHtml = function(active,max){
+    var getActiveGraphHtml = function(usersData){
+        var active = usersData.active;
+        var max = usersData.max;
+        var avg = usersData.avg;
         var percentOfMax = (active/max)*100;
         var strHtml = "";
         var cssClass = "low";
@@ -39,19 +42,48 @@ Dashboard.app = (function(){
         strHtml += "<span class=\"summary "+ getClassFromPercent(percentOfMax) +"\">"+ parseInt(percentOfMax) +"%</span>";
         return strHtml;  
     };
+    var getBarItemHtml = function(max,identifier,label,value){
+        return '<div class="bar bar-'+ identifier +'"><div>&#160;</div><span>'+ value +'</span><span class="country">'+ label +'</span></div>';
+    };
+    var getBarsHtml = function(visitorsByCountry){
+        var max = $(visitorsByCountry).first()[0][1]
+        var strHtml = "";
+        $(visitorsByCountry).each(function(i,elm){
+           console.log(elm + " - " + i);
+           strHtml += getBarItemHtml(max,i+1,elm[0],elm[1]);
+        });
+        $("#bar-chart").html(strHtml);
+        $("#bar-chart .bar").each(function(elm){
+            var elm = $(this);
+            var barBg = $(this).find("div");
+            var val = elm.find("span:first").text();
+            barBg.css("height",val);
+            barBg.css("margin-top",max - val);
+        });
+    };
     return{
         init: function(){
             
         },
-        renderActiveUsersByCountry: function(){
-            renderBar();
+        renderActiveUsersByCountry: function(json){
+            //renderBar();
+            getBarsHtml(json);
         },
-        renderActiveUsersGraph: function(active,max){
-            $("#online_now").html(getActiveGraphHtml(active,max));
+        renderActiveUsersGraph: function(usersData){
+            $("#online-users").html(usersData.active);
+            $("#online-avg").html(usersData.avg);
+            $("#online-max").html(usersData.max);
+            $("#percent-bar").html(getActiveGraphHtml(usersData));
         }
     }
 })();
 $(document).ready(function(){
-    Dashboard.app.renderActiveUsersByCountry();
-    Dashboard.app.renderActiveUsersGraph(800,902);
+    var visitorsByCountry = [["Sweden",487],["Norway",30],["United Kingdom",21],["Denmark",18],["Sri Lanka",17],["Finland",12],["Germany",10],["Poland",10],["Netherlands",7],["India",6]];
+    Dashboard.app.renderActiveUsersByCountry(visitorsByCountry);
+    var activeUsersData = {
+      "active" : 700,
+      "max" : 912,
+      "avg" : 310
+    };
+    Dashboard.app.renderActiveUsersGraph(activeUsersData);
 });
