749b2cd79dbd908f816407690acf5603c28628cd
Jonas Forsberg <himynameisjonas@gmail.com>
2010-12-09 12:03:34 +0100

    render json!

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -2,30 +2,48 @@ class WidgetsController < ApplicationController
   layout false
   
   def active_visits
-    @stats = Chartbeat.quickstats
-    @history = Chartbeat.history
+    content = {
+      :stats => Chartbeat.quickstats,
+      :history => Chartbeat.history  
+    }
+    respond_to do |format|
+      format.json { render :json => content }
+    end
   end
   
   def analytics_goals
     analytics
-    @signup_trials = analytics.signup_trials
-    @signup_follows = analytics.signup_follows
-    @signup_journalists = analytics.signup_journalists
+    content = {
+      :signup_trials => analytics.signup_trials,
+      :signup_follows => analytics.signup_follows,
+      :signup_journalists => analytics.signup_journalists      
+    }
+    respond_to do |format|
+      format.json { render :json => content }
+    end
   end
 
   def count_pressreleases
-    @today = Mynewsdesk.pressreleases_today
-    @yesterday = Mynewsdesk.pressreleases_yesterday
-    @last_week = Mynewsdesk.pressreleases_last_week
+    content = {
+      :today => Mynewsdesk.pressreleases_today,
+      :yesterday => Mynewsdesk.pressreleases_yesterday,
+      :last_week => Mynewsdesk.pressreleases_last_week
+    }
+    respond_to do |format|
+      format.json { render :json => content }
+    end
   end
   
   def message
-    @message = Twitter.get_message
+    content = Twitter.get_message
+    respond_to do |format|
+      format.json { render :json => content }
+    end
   end
   
   def new_relic
     expires_in = Rails.env.production? ? 1.minutes : 10.minutes
-    @values = Rails.cache.fetch("new_relic", :expires_in => expires_in) do
+    content = Rails.cache.fetch("new_relic", :expires_in => expires_in) do
       array = Array.new
       result = NewRelicApi::Account.find(:first).applications(:params => {:conditions => {:name => 'Newsdesk (production)'}}).first.threshold_values
       result.each do |r|
@@ -33,33 +51,56 @@ class WidgetsController < ApplicationController
       end
       array
     end
+    respond_to do |format|
+      format.json { render :json => content }
+    end
   end
   
   def recent_checkins
-    @recent_checkins = Gowalla.recent_checkins
+    content = Gowalla.recent_checkins
+    respond_to do |format|
+      format.json { render :json => content }
+    end   
   end
   
   def recent_pressreleases
-    @recent_pressreleases = Mynewsdesk.recent_pressreleases
+    content = Mynewsdesk.recent_pressreleases
+    respond_to do |format|
+      format.json { render :json => content }
+    end
   end
   
   def recent_referrers
-    @referrers = Chartbeat.referrers[0...10]
+    content = Chartbeat.referrers[0...10]
+    respond_to do |format|
+      format.json { render :json => content }
+    end
   end
   
   def total_visits
     analytics
-    @total_visits = analytics.total_visits
+    content = analytics.total_visits
+    respond_to do |format|
+      format.json { render :json => content }
+    end
   end
   
   def user_stats
-    @followers = Mynewsdesk.followers
-    @journalists = Mynewsdesk.journalists
-    @customers = Mynewsdesk.customers
+    content = {
+      :followers => Mynewsdesk.followers,
+      :journalists => Mynewsdesk.journalists,
+      :customers => Mynewsdesk.customers      
+    }
+    respond_to do |format|
+      format.json { render :json => content }
+    end
   end
   
   def top_countries
-    @top_countries = Chartbeat.countries[0...10]
+    content = Chartbeat.countries[0...10].to_json
+    respond_to do |format|
+      format.json { render :json => content }
+    end
   end
   
   private
